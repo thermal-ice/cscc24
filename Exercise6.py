d59efe2264699672cc37b842c4c9b48af767e570
Carlos Huang <carlos@smilecdr.com>
2022-04-08 23:37:39 -0400

    implemented Matrix, and add (for adding matrices)

--- Exercise6.py
+++ Exercise6.py
@@ -1,3 +1,44 @@
+"""
+Exercise 6: Python operator overloading
+"""
 
-print("hello world!")
 
+class Matrix:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.numRows = len(matrix)
+        self.numCols = len(matrix[0])
+
+    def __str__(self):
+        return f"{self.numRows} x {self.numCols} Matrix:\n" \
+               f"{self.matrix}"
+
+    def __add__(self, other):
+        if self.numCols != other.numCols or self.numRows != other.numRows:
+            raise ValueError("Number of columns and rows in both matrices don't match")
+
+        if isinstance(other, self.__class__):
+            outerArr = []
+            for i in range(self.numRows):
+                tempRowArr = []
+                for j in range(self.numCols):
+                    tempRowArr.append(self.matrix[i][j] + other.matrix[i][j])
+
+                outerArr.append(tempRowArr)
+
+            return Matrix(outerArr)
+
+        if isinstance(other, int):
+            return "will implement later"
+
+
+# Running a script test
+matrix1 = Matrix([[1, 2],
+                  [3, 4]])
+
+matrix2 = Matrix([[2, 2],
+                  [2, 2]])
+
+matrix3 = matrix1 + matrix2
+
+print(matrix3)
